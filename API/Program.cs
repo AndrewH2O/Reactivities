61521325a78d6247f39b6ef810f604c653f195df
AndrewH2O <AndrewH2O@users.noreply.github.com>
2025-02-19 23:56:59 +0000

    Setup EF to store some data for our Activities, we have an activity class in the domain Added AppDbContext, Added sqlite package to Persistence.csproj, Added a conn string to appsettings, Added DbInitializer.cs to seed data and programmatically updating the db via Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 
 var app = builder.Build();
@@ -11,5 +17,25 @@ var app = builder.Build();
 
 // routing - pass requests onto controller
 app.MapControllers();
+
+// create a service scope
+// using will ensure this will be cleaned up
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    
+    // create db via code instead of ef tooling at cmd line
+    // applies any migrations to update the db (creates db if it doesn't exist)
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
+}
 
 app.Run();
