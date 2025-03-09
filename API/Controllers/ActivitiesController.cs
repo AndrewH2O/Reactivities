89bf58101afbbdd5ddcad80ee7a6e39b4f550c89
AndrewH2O <AndrewH2O@users.noreply.github.com>
2025-03-09 12:37:13 +0000

    Added notes on CQRS Added Mediator (Mediatr is package) and implemented GetActivities updating controller and the application layer

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Activities.Queries;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -8,14 +10,14 @@ namespace API.Controllers;
 // api/activities
 
 // primary ctor
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController(AppDbContext context, IMediator mediator) : BaseApiController
 {
     //api/Activities
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
         // request delegate handles this - then it will pass data back to main app thread when done
-        return await context.Activities.ToListAsync();
+        return await mediator.Send(new GetActivityList.Query());
     }
     //api/Activities/{{id}}
     [HttpGet("{id}")]
