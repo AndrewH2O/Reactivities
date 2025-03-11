212cb2a964c2faeccae80deb4ae1b7cec4c5eabf
AndrewH2O <AndrewH2O@users.noreply.github.com>
2025-03-11 10:29:26 +0000

    Add AutoMapper to Application project Add ability to edit an activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -32,4 +32,11 @@ public class ActivitiesController() : BaseApiController
     {
         return await Mediator.Send(new CreateActivity.Command { Activity = activity });
     }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await Mediator.Send((new EditActivity.Command { Activity = activity }));
+        return NoContent(); // 204 no content
+    }
 }
