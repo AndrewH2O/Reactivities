fef753b1a4b1890b47c9e3c32780f3a73c52a17c
AndrewH2O <AndrewH2O@users.noreply.github.com>
2025-03-18 13:06:40 +0000

    Undo cancellation token demo code

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,9 +15,9 @@ public class ActivitiesController() : BaseApiController
 {
     //api/Activities
     [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await Mediator.Send(new GetActivityList.Query(), ct);
+        return await Mediator.Send(new GetActivityList.Query());
     }
     
     //api/Activities/{{id}}
--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -12,24 +12,11 @@ public class GetActivityList
     public class Query:IRequest<List<Activity>>{} 
 
     // returns list of activities
-    public class Handler(AppDbContext context, ILogger<GetActivityList> logger) : IRequestHandler<Query, List<Activity>> 
+    public class Handler(AppDbContext context) : IRequestHandler<Query, List<Activity>> 
     {
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
-            // add some fakery to cause a delay so we may demonstrate use of cancellation token
-            try
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-                    await Task.Delay(1000, cancellationToken);// fake delay
-                    logger.LogInformation($"Task {i} has completed");
-                }
-            }
-            catch (Exception e)
-            {
-                logger.LogInformation("************** Task was cancelled *******************");
-            }
+            
             return await context.Activities.ToListAsync(cancellationToken: cancellationToken);
         }
     }
